987cf6fef91f7c1c35ba915259b3fdcd0aa95868
Zeshan Khalid <imzeshankhalid@gmail.com>
2022-10-31 10:55:42 +0500

    functions are added for input and print values

--- Structs.cpp
+++ Structs.cpp
@@ -4,19 +4,34 @@ struct Person{  // struct with Person attributes
     // Data members ↓
     string name;
     int salary;
+    char Gpa;
 };
+// The following function will read person1's name and salary
+// it will also calculate students Gpa based on marks
+void readIn(Person& person1){
+    int marks;
+    cout<<"Enter Person's name, salary and marks: ";
+    getline(cin,person1.name);
+    cin>>person1.salary>>marks;
+    if(marks>=87)
+        person1.Gpa='A';
+    if(marks>=80)
+        person1.Gpa='B';
+    if(marks>=50)
+        person1.Gpa='C';
+    else
+        person1.Gpa='F';
+}
+// This function will print the person1's values
+void printPerson(Person person1){
+    cout<<"name is "<<person1.name<<endl;
+    cout<<"salary is "<<person1.salary<<endl;
+    cout<<"Gpa is "<<person1.Gpa<<endl;
+}
 int main(){
 
-    Person p1,p2;   // Structure variables
-
-    p1={"ali",5500}; // 1. initializing corresponding values inside struct data members
-    p2.name="nasir"; // 2. another way to initialize value
-    p2.salary=3000;
-
-    // Displaying values ↓
-    cout<<"Name1: "<<p1.name<<endl;
-    cout<<"rno1: "<<p1.salary<<endl;
-    cout<<"Name2: "<<p2.name<<endl;
-    cout<<"rno2: "<<p2.salary<<endl;
-
+    Person p1;
+    // p1 argument is passed as a person1 parameter to the function readIn,PrintPerson
+    readIn(p1);
+    printPerson(p1);
 }

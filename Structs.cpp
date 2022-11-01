f1baca8041f9d96b7bce123ee30c93685dab8b4f
Zeshan Khalid <imzeshankhalid@gmail.com>
2022-11-02 00:00:05 +0500

    arrays added

--- Structs.cpp
+++ Structs.cpp
@@ -8,30 +8,35 @@ struct Person{  // struct with Person attributes
 };
 // The following function will read person1's name and salary
 // it will also calculate students Gpa based on marks
-void readIn(Person& person1){
-    int marks;
-    cout<<"Enter Person's name, salary and marks: ";
-    getline(cin,person1.name);
-    cin>>person1.salary>>marks;
-    if(marks>=87)
-        person1.Gpa='A';
-    if(marks>=80)
-        person1.Gpa='B';
-    if(marks>=50)
-        person1.Gpa='C';
-    else
-        person1.Gpa='F';
+void readIn(Person persons[3],int size){
+    int marks[3];
+    for(int i=0;i<size;i++) {
+        cout << "Enter Person's name, salary and marks: ";
+        getline(cin, persons[i].name);
+        cin >> persons[i].salary >>marks[i];
+        if (marks[i] >= 87)
+            persons[i].Gpa = 'A';
+        if (marks[i] >= 80)
+            persons[i].Gpa = 'B';
+        if (marks[i] >= 50)
+            persons[i].Gpa = 'C';
+        else
+            persons[i].Gpa = 'F';
+    }
 }
 // This function will print the person1's values
-void printPerson(Person person1){
-    cout<<"name is "<<person1.name<<endl;
-    cout<<"salary is "<<person1.salary<<endl;
-    cout<<"Gpa is "<<person1.Gpa<<endl;
+void printPerson(Person persons[3],int size){
+    for(int i=0;i<size;i++){
+    cout<<"name is "<<persons[i].name<<endl;
+    cout<<"salary is "<<persons[i].salary<<endl;
+    cout<<"Gpa is "<<persons[i].Gpa<<endl;
+    }
 }
 int main(){
 
-    Person p1;
+    Person persons[3];
+    int size=3;
     // p1 argument is passed as a person1 parameter to the function readIn,PrintPerson
-    readIn(p1);
-    printPerson(p1);
+    readIn(persons[3],size);
+    printPerson(persons[3],size);
 }

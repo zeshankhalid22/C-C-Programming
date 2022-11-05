0e30ed52f71db09bb93341650e7b4666c5ae40cb
Zeshan Khalid <imzeshankhalid@gmail.com>
2022-11-05 10:40:18 +0500

    Pattern 1 added

--- triAngularLoopPattern.cpp
+++ triAngularLoopPattern.cpp
@@ -1,5 +1,21 @@
 #include "iostream"
 using namespace std;
 // * Outer loop decides no of Rows, Inner loop decides no of columns
+void pattern1(){
+    cout<<"Enter No of Rows: ";
+    int n,totalColInRow;
+    cin>>n;
+    for(int row=1;row<=n;row++){
+        if(row<=n/2)
+            totalColInRow=row;
+        else if(row>n/2)
+            totalColInRow=n-row;
+        for(int col=1;col<=totalColInRow;col++){
+            cout<<"* ";
+        }
+        cout<<"\n";
+    }
+}
 int main(){
+    pattern1();
 }

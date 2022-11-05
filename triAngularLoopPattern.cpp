2b09ca9073f42175d4911cbdba8271229ce6e0cd
Zeshan Khalid <imzeshankhalid@gmail.com>
2022-11-05 18:31:45 +0500

    pyramid pattern added

--- triAngularLoopPattern.cpp
+++ triAngularLoopPattern.cpp
@@ -2,18 +2,23 @@
 using namespace std;
 // * Outer loop decides no of Rows, Inner loop decides no of columns
 void pattern1(){
-    cout<<"Enter No of Rows: ";
-    int n,totalColInRow;
-    cin>>n;
-    for(int row=1;row<=n;row++){
-        if(row<=n/2)
-            totalColInRow=row;
-        else if(row>n/2)
-            totalColInRow=n-row;
-        for(int col=1;col<=totalColInRow;col++){
-            cout<<"* ";
+    int n;
+    cin>>n; // if n=4,  r=(1→4) 3, c=(1→7)
+    // * Formula: for(col>=(n-r-1) to col<=(n+r-1)) print "*"
+    /*    r | n-3 n-2 n-1 n n+1 n+2 n+3
+          1 |             *
+          2 |          *  *  *
+          3 |      *   *  *  *   *
+          4 |   *  *   *  *  *   *  *
+         */
+    for(int r=1;r<=n;r++){
+        for(int col=1;col<=(2*n)-1;col++){
+            if(col >= n - (r -1) && col <= n + (r - 1))
+                cout<<"*";
+            else
+                cout<<" ";
         }
-        cout<<"\n";
+        cout<<endl;
     }
 }
 int main(){
